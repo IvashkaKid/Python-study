3ec0768621e418c506abdca422611fb00df4061b
IvashkaKid <125472587+IvashkaKid@users.noreply.github.com>
2023-03-30 01:25:45 +0300

    Update Third.py

--- Third.py
+++ Third.py
@@ -1,29 +1,29 @@
-string = str(input())
+while True:
+    string = str(input("Введите текст, слова которого разделённы пробелами: "))
 
-max_popular = 0
-popular = 0
-word_popular = ""
-word_max_len = ""
-max_len = 0
-
-array = string.split(" ")
-
-for i in array:
-    
+    max_popular = 0
     popular = 0
+    word_popular = ""
+    word_max_len = ""
+    max_len = 0
 
-    if len(i) > max_len:
-        max_len = len(i)
-        word_max_len = i
+    array = string.split(" ")
+
+    for i in array:
+    
+        popular = 0
+
+        if len(i) > max_len:
+            max_len = len(i)
+            word_max_len = i
         
-    for j in array:
-        if i == j:
-            popular += 1
+        for j in array:
+            if i == j:
+                popular += 1
             
-    if popular > max_popular:
-        word_popular = i
-        max_popular = popular
+        if popular > max_popular:
+            word_popular = i
+            max_popular = popular
         
-print("Наиболее встречающееся слово: ",word_popular)
-print("Наиболее длинное слово: ",word_max_len)
-
+    print("Наиболее встречающееся слово: ",word_popular)
+    print("Наиболее длинное слово: ",word_max_len,"\n")
