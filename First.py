6eac6c6fe65feae81ba74454253c576fa1275f8c
IvashkaKid <125472587+IvashkaKid@users.noreply.github.com>
2023-03-30 01:01:49 +0300

    Update First.py

--- First.py
+++ First.py
@@ -1,9 +1,12 @@
-number_one = int(input())
-number_two = int(input())
+while True:
+    number_one = float(input("Введите первое число: "))
+    number_two = float(input("Введите второе число: "))
 
-if number_one < number_two:
-    print("Первое число меньше второго")
-elif number_one > number_two:
-    print("Первое число больше второго", end=' ')
-    if number_one >= (number_two * 3):
-        print("в 3 раза")
+    if number_one < number_two:
+        print("Первое число меньше второго\n")
+    elif number_one > number_two:
+        print("Первое число больше второго", end=' ')
+        if number_one >= (number_two * 3):
+            print("в более чем 3 раза\n")
+        else:
+            print("\n")
